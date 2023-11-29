8d6a7ac1307018dc23dd34f32674dcc01c5244cc
後藤健一郎 <gotokenichiro@gotoukenichirounoMacBook-Air.local>
2023-11-29 20:06:29 +0900

    見た目を少しだけ変更した

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,50 +3,56 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <section className="mb-16">
-          <h1 className="text-4xl font-bold mb-4">私の経歴</h1>
-          <p className="text-lg mb-4">
-            2023年3月31日愛知県立刈谷高等学校卒業<br/>
-            2023年4月1日立命館大学入学
-          </p>
-        </section>
+      <nav className="w-full flex justify-between items-center mb-16">
+        <a href="#profile">プロフィール</a>
+        <a href="#skills">スキル</a>
+        <a href="#works">作品</a>
+        <a href="#articles">記事</a>
+        <a href="#presentations">プレゼンテーション</a>
+        <a href="#contact">連絡先</a>
+      </nav>
 
-        <section className="mb-16">
-          <h1 className="text-4xl font-bold mb-4">私のスキル</h1>
-          <p className="text-lg mb-4">
-            2023年4月TOEIC 630点
-          </p>
-        </section>
+      <section id="profile" className="mb-16">
+        <h1 className="text-4xl font-bold mb-4">プロフィール</h1>
+        <p className="text-lg mb-4">
+          ここにあなたのプロフィールを書いてください。
+        </p>
+      </section>
 
-        <section className="mb-16">
-          <h1 className="text-4xl font-bold mb-4">私のプロジェクト</h1>
-          <p className="text-lg mb-4">
-            ここにあなたのプロジェクトの詳細を書いてください。
-          </p>
-        </section>
+      <section id="skills" className="mb-16">
+        <h1 className="text-4xl font-bold mb-4">スキル</h1>
+        <p className="text-lg mb-4">
+          ここにあなたのスキルを書いてください。
+        </p>
+      </section>
 
-        <section className="mb-16">
-          <h1 className="text-4xl font-bold mb-4">私の趣味</h1>
-          <p className="text-lg mb-4">
-            ここにあなたの趣味や興味を書いてください。
-          </p>
-        </section>
+      <section id="works" className="mb-16">
+        <h1 className="text-4xl font-bold mb-4">作品</h1>
+        <p className="text-lg mb-4">
+          ここにあなたの作品を書いてください。
+        </p>
+      </section>
 
-        <section className="mb-16">
-          <h1 className="text-4xl font-bold mb-4">受賞歴</h1>
-          <p className="text-lg mb-4">
-            ここにあなたの受賞歴を書いてください。
-          </p>
-        </section>
+      <section id="articles" className="mb-16">
+        <h1 className="text-4xl font-bold mb-4">記事</h1>
+        <p className="text-lg mb-4">
+          ここにあなたの記事を書いてください。
+        </p>
+      </section>
 
-        <section className="mb-16">
-          <h1 className="text-4xl font-bold mb-4">連絡先</h1>
-          <p className="text-lg mb-4">
-            ここにあなたの連絡先を書いてください。
-          </p>
-        </section>
-      </div>
+      <section id="presentations" className="mb-16">
+        <h1 className="text-4xl font-bold mb-4">プレゼンテーション</h1>
+        <p className="text-lg mb-4">
+          ここにあなたのプレゼンテーションを書いてください。
+        </p>
+      </section>
+
+      <section id="contact" className="mb-16">
+        <h1 className="text-4xl font-bold mb-4">連絡先</h1>
+        <p className="text-lg mb-4">
+          ここにあなたの連絡先を書いてください。
+        </p>
+      </section>
     </main>
   )
 }
